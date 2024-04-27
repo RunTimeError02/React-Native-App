dda56c6f8810ddeb1f2d55144d8c018fa1ac0c5d
RunTimeError02 <102181573+RunTimeError02@users.noreply.github.com>
2024-04-27 15:21:58 +0530

    Login/Register Done

--- api/index.js
+++ api/index.js
@@ -6,15 +6,24 @@ const LocalStrategy = require("passport-local").Strategy
 
 
 const app = express();
-const port = 8000;
+const port = 3001;
 const cors = require("cors");
 app.use(cors());
-
+app.use(express.json())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 const jwt = require("jsonwebtoken");
+// app.use(cors({origin: true, credentials: true}));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+// app.options("*", cors({ origin: 'http://localhost:8000', optionsSuccessStatus: 200 }));
+// app.use(cors({ origin: "http://localhost:8000", optionsSuccessStatus: 200 }));
 
 mongoose.connect(
     "mongodb+srv://sagardave005:2003@cluster0.vnmu3e9.mongodb.net/", {
@@ -41,7 +50,7 @@ app.post("/register", (req, res) => {
 
   // create a new User object
   const newUser = new User({ name, email, password, image });
-
+  
   // save the user to the database
   newUser
     .save()
@@ -53,3 +62,51 @@ app.post("/register", (req, res) => {
       res.status(500).json({ message: "Error registering the user!" });
     });
 });
+
+ //function to create a token for the user
+ const createToken = (userId) => {
+  // Set the token payload
+  const payload = {
+    userId: userId,
+  };
+
+  // Generate the token with a secret key and expiration time
+  const token = jwt.sign(payload, "Q$r2K6W8n!jCW%Zk", { expiresIn: "1h" });
+
+  return token;
+};
+
+//endpoint for logging in of that particular user
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+  
+    //check if the email and password are provided
+    if (!email || !password) {
+      return res
+        .status(404)
+        .json({ message: "Email and the password are required" });
+    }
+    
+    //check for that user in the database
+    User.findOne({ email })
+      .then((user) => {
+        if (!user) {
+          //user not found
+          return res.status(404).json({ message: "User not found" });
+        }
+  
+        //compare the provided passwords with the password in the database
+        if (user.password !== password) {
+          return res.status(404).json({ message: "Invalid Password!" });
+        }
+  
+        const token = createToken(user._id);
+        res.status(200).json({ token });
+      })
+      .catch((error) => {
+        console.log("error in finding the user", error);
+        res.status(500).json({ message: "Internal server Error!" });
+      });
+  });
+
+ 
